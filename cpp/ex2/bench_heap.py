f76b35ad271ecebda56cdbd2b9286a190140d662
Leo Chatain <leochatain@gmail.com>
2012-07-17 17:06:01 -0300

    More

--- cpp/ex2/bench_heap.py
+++ cpp/ex2/bench_heap.py
@@ -5,7 +5,11 @@ import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from math import log
 import os
+
+def inv_log_log(x):
+  return 1 / log(log(x, 2), 2)
 
 num_benchs = 10
 
@@ -24,14 +28,31 @@ for i in xrange(10, 20):
 
 plt.subplot(111)
 
-plt.ylabel('Size');
-plt.title('Time(s)')
+plt.ylabel('Time(s)')
+plt.xlabel('Size')
+plt.title('VEB Benchmark')
 plt.xticks(sizes, rotation=30, size='small')
 plt.grid(True)
 
 plt.plot(sizes, push, 'r--', label='serial')
 plt.plot(sizes, pop, 'b--', label='acc')
 plt.plot(sizes, update, 'g--', label='cublas')
+plt.legend(loc="upper left")
+
+plt.savefig('heap.png')
+
+# Plot inverse
+plt.subplot(111)
+
+plt.ylabel('Time(s)')
+plt.xlabel('Size')
+plt.title('VEB Benchmark Normalized')
+plt.xticks(sizes, rotation=30, size='small')
+plt.grid(True)
+
+plt.plot(sizes, map(inv_log_log, push), 'r--', label='serial')
+plt.plot(sizes, map(inv_log_log, pop), 'b--', label='acc')
+plt.plot(sizes, map(inv_log_log, update), 'g--', label='cublas')
 plt.legend(loc="upper left")
 
 plt.savefig('heap.png')
