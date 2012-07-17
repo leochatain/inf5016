60790ae156e116fef8045ffbfdb0b14c3de7bfb6
Leo Chatain <leochatain@gmail.com>
2012-07-17 13:12:11 -0300

    new version

--- cpp/ex2/bench_dij.py
+++ cpp/ex2/bench_dij.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import os
 
 def generate(size):
-  print 'generating for', size
   os.system('../files/gen ' + str(size) + ' 1 > ../files/' + str(size) + '.gr')
 
 num_benchs = 10
@@ -17,7 +16,7 @@ sizes = []
 times = []
 
 print '# x f(x)'
-for i in xrange(5, 20):
+for i in xrange(5, 15):
   size = 2 ** i
   sname = str(size)
   graph_file = '../files/' + str(sname) + '.gr'
