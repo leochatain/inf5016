867988333b1df48c8f78bf03a7928ab6ca6bba36
Leo Chatain <leochatain@gmail.com>
2012-07-17 11:53:10 -0300

    new script

--- cpp/ex2/bench_heap.py
+++ cpp/ex2/bench_heap.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python
+#Author: leochatain@gmail.com (Leo Chatain)
+
+import sys
+import matplotlib.pyplot as plt
 import os
 
 num_benchs = 10
 
+sizes = []
 push = []
 pop = []
 update = []
 
-for size in xrange(2**14, 2**18, 2**14):
+for i in xrange(8, 10):
+  size = 2 ** i
   stream = os.popen('./bin/bench_heap -b ' + str(num_benchs) + ' -s ' + str(size))
-  push.append((size, stream.next().rstrip()))
-  pop.append((size, stream.next().rstrip()))
-  update.append((size, stream.next().rstrip()))
+  sizes.append(size)
+  push.append(stream.next().rstrip())
+  pop.append(stream.next().rstrip())
+  update.append(stream.next().rstrip())
+
+plt.subplot(111)
+
+plt.ylabel('Size');
+plt.title('Time(s)')
+plt.xticks(sizes, rotation=30, size='small')
+plt.grid(True)
+
+plt.plot(sizes, push, 'r--', label='serial')
+plt.plot(sizes, pop, 'bs', label='acc')
+plt.plot(sizes, update, 'g^', label='cublas')
+
+plt.savefig('heap.png')
 
 print '# x f(x)'
-for t in push:
-  print t[0], t[1]
+for s, t in sizes, push:
+  print s, t
 print '# x f(x)'
-for t in pop:
-  print t[0], t[1]
+for s, t in sizes, pop:
+  print s, t
 print '# x f(x)'
-for t in update:
-  print t[0], t[1]
+for s, t in sizes, update:
+  print s, t
 
 
