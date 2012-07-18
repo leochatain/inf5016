8566760c07adbccebc0be93ceb59b25ec6ddec24
Leo Chatain <leochatain@gmail.com>
2012-07-17 21:37:09 -0300

    from ex1

--- cpp/ex1/bench_dij.py
+++ cpp/ex1/bench_dij.py
@@ -2,15 +2,15 @@
 import os
 
 def generate(size):
-  os.system('bin/gen ' + str(size) + ' 1 > files/' + str(size) + '.gr')
+  os.system('bin/gen ' + str(size) + ' 1 > ../files/' + str(size) + '.gr')
 
 num_benchs = 10
 
 print '# x f(x)'
-for i in xrange(10240 + 1024, 2**14 +1, 1024):
+for i in xrange(512, 8192+512, 512):
   size = i
   sname = str(size)
-  graph_file = 'files/' + str(sname) + '.gr'
+  graph_file = '../files/' + str(sname) + '.gr'
 
   if not os.path.exists(graph_file):
     generate(size)
