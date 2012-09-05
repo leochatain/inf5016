e34adc3e5d3eff6135141eb5b332f4ba2ee5ed0a
Leo Chatain <leochatain@gmail.com>
2012-09-05 19:50:28 -0300

    Updated .gitignore to add vim

--- ex1/src/main.cpp
+++ ex1/src/main.cpp
@@ -6,18 +6,13 @@
 using std::cout;
 using std::endl;
 
-using std::vector;
+using namespace inf5016;
 
 int main() {
   cout << "Hello World" << endl;
-
-  vector<int> boo;
 
   inf5016::BinaryHeap<int, int> heap;
 
   heap.push(1, 2);
   heap.update(1, 2);
-
-
-
 }
