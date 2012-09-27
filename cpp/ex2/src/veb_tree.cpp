000a62f880cfc338274472318e6ceadba7fe106a
Leo Chatain <leochatain@gmail.com>
2012-09-27 16:28:18 -0300

    Add useless comments on header.

--- cpp/ex2/src/veb_tree.cpp
+++ cpp/ex2/src/veb_tree.cpp
@@ -1,3 +1,16 @@
 #include "veb_tree.h"
 
-using namespace inf5016;
+namespace inf5016 {
+
+VebTree::VebTree() {}
+void VebTree::push(const Edge& edge) {}
+void VebTree::pop() {}
+Edge& VebTree::top() {}
+const Edge& VebTree::top() const {}
+void VebTree::update(const int vert, const int new_cost) {}
+int VebTree::size() {}
+bool VebTree::empty() {}
+void VebTree::clean() {}
+
+}
+
--- cpp/ex2/src/veb_tree.h
+++ cpp/ex2/src/veb_tree.h
@@ -8,17 +8,28 @@ class VebTree {
  public:
   VebTree();
 
+  // Check whether a vertex is member of the tree.
+  bool member(const int vert);
+  const bool member(const int vert) const;
+
+  // Pushes an edge to the tree.
   void push(const Edge& e);
 
+  // Pops the min element of the tree.
   void pop();
 
+  // Updates the value of vert with new_cost.
   void update(const int vert, const int new_cost);
 
-  inline Edge& top();
+  // Queries the top value.
+  Edge& top();
   const Edge& top() const;
 
+  // The number of elements in the tree.
   int size();
+  // Is the tree empty.
   bool empty();
+  // Removes all elements from the tree.
   void clean();
 
  private:
