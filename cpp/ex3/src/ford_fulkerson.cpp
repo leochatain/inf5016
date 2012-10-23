4bb86868ebcae14f2a323d7da7a7b1f6cba14b86
Leo Chatain <leochatain@gmail.com>
2012-10-22 23:30:59 -0200

    Retab

--- cpp/ex3/src/ford_fulkerson.cpp
+++ cpp/ex3/src/ford_fulkerson.cpp
@@ -30,27 +30,27 @@ Graph FordFulkerson::create_residual_graph(const Graph& graph) {
     }
   }
 
-	return residual;
+  return residual;
 }
 
 // Updates the residual graph with the augmenting path. Returns the
 // width of the path.
 // TODO(leochatain): this should really be a pfs, rather than a bfs.
 int FordFulkerson::pfs(Graph& residual, const int src, const int dst) {
-	queue<Edge> q;
-	for (int i = 0; i < residual[src].size(); i++) {
-		q.push(residual[src][i]);
-	}
+  queue<Edge> q;
+  for (int i = 0; i < residual[src].size(); i++) {
+    q.push(residual[src][i]);
+  }
 
-	while (!q.empty()) {
-		const Edge& cur = q.front();
-		q.pop();
-		if (cur.dest == dst) {
-			// we're home boys
-		} else {
-			// queue up these guys over here.
-		}
-	}
+  while (!q.empty()) {
+    const Edge& cur = q.front();
+    q.pop();
+    if (cur.dest == dst) {
+      // we're home boys
+    } else {
+      // queue up these guys over here.
+    }
+  }
 }
 
 }
