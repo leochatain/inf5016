ba81ba4303f079c207be7b93228076155c365a8c
Leo Chatain <leochatain@gmail.com>
2012-12-21 01:56:57 -0200

    Comply with DIMACS

--- cpp/ex3/src/main.cpp
+++ cpp/ex3/src/main.cpp
@@ -10,28 +10,25 @@
 using namespace std;
 using namespace inf5016;
 
-Graph parse_graph(istream& in);
+Graph parse_graph(istream& in, int& s, int& t);
 void print_help();
 
 int main(int argc, char** argv) {
-  if (argc < 3 ) {
-    print_help();
-    return 0;
-  }
-
-  int src = atoi(argv[1]);
-  int dst = atoi(argv[2]);
-
+  int s = -1, t = -1;
   // Parse graph and benchmarks.
-  const Graph graph = parse_graph(cin);
+  const Graph graph = parse_graph(cin, s, t);
+
+  if (s == -1 || t == -1) {
+    cout << "Source or sink are not defined" << endl;
+  }
 
   FordFulkerson ff(graph);
 
-  int max_flow = ff.run(src, dst);
+  int max_flow = ff.run(s, t);
   cout << max_flow << endl;
 }
 
-Graph parse_graph(istream& in) {
+Graph parse_graph(istream& in, int& s, int& t) {
   Graph g;
   string line;
 
@@ -40,6 +37,14 @@ Graph parse_graph(istream& in) {
       int u, v, w;
       sscanf(line.c_str(), "a %d %d %d", &u, &v, &w);
       g[u][v] = w;
+    } else if (line[0] == 'n') {
+      char which;
+      int val;
+      sscanf(line.c_str(), "n %d %c", &val, &which);
+      if (which == 's')
+        s = val;
+      else
+        t = val;
     }
   }
 
