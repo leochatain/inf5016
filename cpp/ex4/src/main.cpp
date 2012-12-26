e8789500b7993c45dc5448f331104e730a8a7f4b
Leo Chatain <leochatain@gmail.com>
2012-12-26 00:03:50 -0200

    Add parse graph method

--- cpp/ex4/src/main.cpp
+++ cpp/ex4/src/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <cassert>
 
 #include "hopcroft_karp.h"
 
@@ -20,4 +21,21 @@ int main(int argc, char** argv) {
 }
 
 Graph parse_graph(istream& in, set_i& s, set_i& t) {
+  Graph g;
+  string line;
+
+  while (getline(in, line)) {
+    if (line[0] == 'a') {
+      int u, v;
+      sscanf(line.c_str(), "a %d %d", &u, &v);
+      g[u].insert(v);
+      s.insert(u);
+      t.insert(v);
+
+      assert(s.find(v) == s.end());
+      assert(t.find(u) == t.end());
+    }
+  }
+
+  return g;
 }
